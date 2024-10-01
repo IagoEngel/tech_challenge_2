2be273160292700c0b89f4e8f0b52744a5d2340f
Iago Engel Serafim <iagoengel@yahoo.com>
2024-09-30 23:10:27 -0300

    Access-Control-Allow-Origin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { HttpExceptionFilter } from './shared/filters/http-exception';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
   await app.listen(Number(process.env.PORT) || 3010);
 }
 bootstrap();
